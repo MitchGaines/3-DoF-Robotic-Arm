e106350a74674309f1120d53cc111053c1bb207c
mhoff2016 <48568665+mhoff2016@users.noreply.github.com>
2019-09-24 20:59:54 -0400

    Updated jacob0 to full functionality

--- jacob0.m
+++ jacob0.m
@@ -1,63 +1,32 @@
 function J = jacob0(q)
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
-
- DH = tdh(-q(1), 0.135, 0, -90) * tdh(-q(2), 0, 0.175, 0) * tdh(-q(3)+90, 0, 0.16928, 90);
- 
- x_vect = [DH(1,4), DH(2,4), DH(3,4)];
- 
- [J1] = gradient(x_vect, q(1));
- [J2] = gradient(x_vect, q(2));
- [J3] = gradient(x_vect, q(3));
- 
- J = zeros(6, 3);
- J(1,1) = J1(1);
- J(2,1) = J1(2);
- J(3,1) = J1(3);
- 
- J(1,2) = J2(1);
- J(2,2) = J2(2);
- J(3,2) = J2(3);
- 
- J(1,3) = J3(1);
- J(2,3) = J3(2);
- J(3,3) = J3(3);
- 
-%  J = zeros(6, 3);
-%  J(1,1) = gradient(x_vect(1), x);
-%  J(2,1) = gradient(x_vect(2), x);
-%  J(3,1) = gradient(x_vect(3), x);
-%  
-%  J(1,2) = gradient(x_vect(1), y);
-%  J(2,2) = gradient(x_vect(2), y);
-%  J(3,2) = gradient(x_vect(3), y);
-%  
-%  J(1,3) = gradient(x_vect(1), z);
-%  J(2,3) = gradient(x_vect(2), z);
-%  J(3,3) = gradient(x_vect(3), z);
- 
-  x = q(1);
-  y = q(2);
-  z = q(3);
-
  
  
+ theta_1 = q(1)*pi/180;
+ theta_2 = q(2)*pi/180;
+ theta_3 = q(3)*pi/180;
+ L1 = 0.135; L2 = 0.175; L3 = 0.16928;
+ JV = [L3*sin(theta_1)*sin(theta_2)*sin(theta_3) - L3*cos(theta_2)*cos(theta_3)*sin(theta_1) - L2*cos(theta_2)*sin(theta_1), - L2*cos(theta_1)*sin(theta_2) - L3*cos(theta_1)*cos(theta_2)*sin(theta_3) - L3*cos(theta_1)*cos(theta_3)*sin(theta_2), - L3*cos(theta_1)*cos(theta_2)*sin(theta_3) - L3*cos(theta_1)*cos(theta_3)*sin(theta_2); L3*cos(theta_1)*sin(theta_2)*sin(theta_3) - L3*cos(theta_1)*cos(theta_2)*cos(theta_3) - L2*cos(theta_1)*cos(theta_2), L2*sin(theta_1)*sin(theta_2) + L3*cos(theta_2)*sin(theta_1)*sin(theta_3) + L3*cos(theta_3)*sin(theta_1)*sin(theta_2), L3*cos(theta_2)*sin(theta_1)*sin(theta_3) + L3*cos(theta_3)*sin(theta_1)*sin(theta_2); sym(0), L2*cos(theta_2) - L3*cos(theta_2)*cos(theta_3) - L3*sin(theta_2)*sin(theta_3), L3*cos(theta_2)*cos(theta_3) + L3*sin(theta_2)*sin(theta_3)];
+ JV = double(vpa(JV,4));
+  
   T0_1 = tdh(-q(1), 0.135, 0, -90);
   T0_2 = tdh(-q(1), 0.135, 0, -90) * tdh(-q(2), 0, 0.175, 0);
   
   % z0
-  J(4,1) = 0;
-  J(5,1) = 0;
-  J(6,1) = 1;
+  JW(1,1) = 0;
+  JW(2,1) = 0;
+  JW(3,1) = 1;
   % z1
-  J(4,2) = T0_1(1,3); 
-  J(5,2) = T0_1(2,3);
-  J(6,2) = T0_1(3,3);
+  JW(1,2) = T0_1(1,3); 
+  JW(2,2) = T0_1(2,3);
+  JW(3,2) = T0_1(3,3);
   %z2
-  J(4,3) = T0_2(1,3); 
-  J(5,3) = T0_2(2,3);
-  J(6,3) = T0_2(3,3);
+  JW(1,3) = T0_2(1,3); 
+  JW(2,3) = T0_2(2,3);
+  JW(3,3) = T0_2(3,3);
   
+  J = [JV;JW];
 
  
 
