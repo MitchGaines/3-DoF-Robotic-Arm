86f62186f6373abca85d0be5460d9a0b5a8a1c85
Mitch Gaines <gainesmitch2@gmail.com>
2019-10-10 09:29:09 -0400

    final touches

--- src/getCheckerboardToRobot.m
+++ src/getCheckerboardToRobot.m
@@ -4,7 +4,7 @@ function [ converted_locations ] = getCheckerboardToRobot(locations)
     converted_locations = locations;
     for row=1:size(locations, 1)
         converted_locations(row,1) = locations(row, 1) + 253.8;
-        converted_locations(row,2) = locations(row, 2) - 126.4;
+        converted_locations(row,2) = locations(row, 2) - 122.4;
     end
 end
 
